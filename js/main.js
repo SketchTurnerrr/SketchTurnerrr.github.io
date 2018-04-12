7f911717bf1fde9ac96ba1841840345f2dbc6207
SketchTurnerrr <nareknarek45@gmail.com>
2018-04-12 15:07:51 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//Sidenav Left
+function openNav() {
+    document.getElementById("side-navbar").style.width = "300px";
+}
+
+function closeNav() {
+    document.getElementById("side-navbar").style.width = "0";
+}
+
+// Sidenav Right
+function openNavRight() {
+    document.getElementById("side-navbar-right").style.width = "300px";
+}
+
+function closeNavRight() {
+    document.getElementById("side-navbar-right").style.width = "0";
+}
